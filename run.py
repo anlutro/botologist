21b9714bcf04e834298439393cd573000cc8dc5b
Andreas Lutro <anlutro@gmail.com>
2014-08-09 22:50:36 +0200

    reorganize a bit

--- run.py
+++ run.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 import os.path
+from sys import argv
 
 from ircbot import run_bot
 
-if __name__ == '__main__':
+def main():
+	if len(argv) < 4:
+		print('Usage: ./run.py <server> <channel> <nick>')
+		return
+
 	cwd = os.path.dirname(os.path.realpath(__file__))
 	storage_path = os.path.join(cwd, 'storage')
 
 	run_bot(
-		server='irc.quakenet.org',
+		server=argv[1],
 		port=6667,
-		channel='#rzbot',
-		nick='rzbot',
+		channel=argv[2],
+		nick=argv[3],
 		storage_path=storage_path
 	)
+
+if __name__ == '__main__':
+	main()
