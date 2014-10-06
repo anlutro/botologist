f72a78d298b0ac00b45c69c79519502c686f68f1
David Trail <napalm@trailbeans.eu>
2014-10-06 23:57:47 +0100

    Much better syntax, fixed a couple of bugs, uses OO style, sorts by score, syntactically more appealing

--- ircbot/plugin/qdb_search.py
+++ ircbot/plugin/qdb_search.py
@@ -1,52 +1,67 @@
 import json
 import urllib.request
+import operator
 from datetime import datetime, timedelta
 
 _BASE_URL = "http://bash.vaunt.eu/"
 _JSON_URL = "{base_url}?json".format(base_url=_BASE_URL)
-_URL_FORMAT_STRING = "{base_url}?{id}"
-_MAX_SEARCH_RESULTS = 3
-_QUOTES = []
-_LAST_UPDATE = datetime.now()
+
 
 class Quote:
-	"""Base Quote class."""
-	id = None
-	text = None
-	score = None
+    """Base Quote class."""
+    id = None
+    text = None
+    score = None
 
-	def __init__(self, _hash):
-		self.id = _hash['id']
-		self.text = _hash['quote']
-		self.score = _hash['popularity']
+    def __init__(self, _hash):
+        self.id = _hash['id']
+        self.text = _hash['quote'].lower()
+        self.score = int(_hash['popularity'])
 
-	def __str__(self):
-		return """#{id}: {url} ({score})""".format(
-			id=self.id,
-			score=self.score,
-			url=_URL_FORMAT_STRING.format(base_url=_BASE_URL, id=self.id),
-		)
-
-
-def update_quotes():
-	def sort_by_score(quote):
-		"""Sorts by score, we want the highest voted prioritised."""
-		return quote.score
-	response = urllib.request.urlopen(_JSON_URL)
-	json_quotes = json.loads(response.read().decode('utf-8'))
-	for j in json_quotes:
-		quote = Quote(j)
-		_QUOTES.append(quote)
-	return sorted(_QUOTES, key=sort_by_score, reverse=True)
+    def __str__(self):
+        return """#{id}: {url} ({score})""".format(
+            id=self.id,
+            score=self.score,
+            url="{base_url}?{id}".format(
+                base_url=_BASE_URL,
+                id=self.id,
+                )
+            )
 
 
-def search_quotes(string):
-	"""List of quotes matching search string."""
-	if not _QUOTES or datetime.now() - _LAST_UPDATE > timedelta(minutes=30):
-		update_quotes()
-	matches = []
-	for quote in _QUOTES:
-		if string in quote.text and len(matches) < _MAX_SEARCH_RESULTS:
-			matches.append(quote)
-	return matches
+class Quotes:
+    _max_results = 5  # Max returned quotes.
 
+    def __init__(self):
+        self._last_update = datetime.now()
+        self._quotes = self.update_quotes()
+
+
+    def update_quotes(self):
+        """Fetches and sorts our quoties."""
+        response = urllib.request.urlopen(_JSON_URL)
+        json_quotes = json.loads(response.read().decode('utf-8'))
+        quotes = []
+        for j in json_quotes:
+            quote = Quote(j)
+            quotes.append(quote)
+        self._last_update = datetime.now()
+        return self.ordered_quotes(quotes)
+
+
+    def search_quotes(self, string):
+        """List of quotes matching search string."""
+        if not self._quotes or datetime.now() - self._last_update > timedelta(minutes=30):
+            self._quotes = self.update_quotes()
+        matches = []
+        for quote in self._quotes:
+            if string in quote.text and len(matches) < self._max_results:
+                matches.append(quote)
+        return matches
+
+
+    def ordered_quotes(self, quotes):
+        """Reverse ordered quotes by score."""
+        return sorted(quotes, key=operator.attrgetter('score'), reverse=True)
+
+# vim: ts=4
