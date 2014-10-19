6fffe977af952ac239a87c13eb5205df57a3781f
Andreas Lutro <anlutro@gmail.com>
2014-10-19 16:01:11 +0200

    fix gay here reply

--- ircbot/replies.py
+++ ircbot/replies.py
@@ -3,5 +3,11 @@ def tableflip(bot, message, nick):
 		return '┬─┬ ノ( ゜-゜ノ)'
 
 def nay_here(bot, message, nick):
-	if 'nay' in nick.lower() and 'sup' in message.split():
+	if 'nay' not in nick.lower():
+		return
+
+	# remove all non-ascii characters
+	msg = ''.join([i if ord(i) < 128 else '' for i in message.strip()])
+
+	if 'sup' in msg.split() or 'yo' == msg:
 		return 'gay here'
