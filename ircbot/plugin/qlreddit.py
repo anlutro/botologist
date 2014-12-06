71e20ab56a001387496af72b67a467c02f046871
Andreas Lutro <anlutro@gmail.com>
2014-12-06 17:21:25 +0100

    add locomotion

--- ircbot/plugin/qlreddit.py
+++ ircbot/plugin/qlreddit.py
@@ -7,3 +7,8 @@ class QlredditPlugin(ircbot.plugin.Plugin):
 	def opa_opa(self, msg):
 		if 'opa opa' in msg.message.lower():
 			return 'https://www.youtube.com/watch?v=Dqzrofdwi-g'
+
+	@ircbot.plugin.reply
+	def locomotion(self, msg):
+		if 'locomotion' in msg.message.lower():
+			return 'https://www.youtube.com/watch?v=dgjc-6L0Wm4#t=5'
