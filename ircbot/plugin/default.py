bc586f706008c4d212f49f1865df32cd673a2206
Andreas Lutro <anlutro@gmail.com>
2015-07-02 12:25:54 +0200

    flags go on .compile, not .match

--- ircbot/plugin/default.py
+++ ircbot/plugin/default.py
@@ -20,21 +20,21 @@ class DefaultPlugin(ircbot.plugin.Plugin):
 			return '┬─┬ ノ( ゜-゜ノ)'
 
 	insults = (
-		(re.compile(r'.*fuck(\s+you)\s*,?\s*'+cfg['bot']['nick']+'.*'),
+		(re.compile(r'.*fuck(\s+you)\s*,?\s*'+cfg['bot']['nick']+'.*', re.IGNORECASE),
 		'fuck you too {nick}'),
-		(re.compile(r'.*'+cfg['bot']['nick']+'[,:]?\s+fuck\s+you.*'),
+		(re.compile(r'.*'+cfg['bot']['nick']+'[,:]?\s+fuck\s+you.*', re.IGNORECASE),
 		'fuck you too {nick}'),
 	)
 
 	@ircbot.plugin.reply()
 	def return_insults(self, msg):
 		for expr, reply in self.insults:
-			if expr.match(msg.message, re.IGNORECASE):
+			if expr.match(msg.message):
 				return reply.format(nick=msg.user.nick)
 
-	no_work = re.compile(r".*(__)?bot(__)?\s+(no|not|doesn.?t|does not)\s+work.*")
+	no_work = re.compile(r".*(__)?bot(__)?\s+(no|not|doesn.?t|does not)\s+work.*", re.IGNORECASE)
 
 	@ircbot.plugin.reply()
 	def bot_always_works(self, msg):
-		if self.no_work.match(msg.message, re.IGNORECASE):
+		if self.no_work.match(msg.message):
 			return 'I always work'
