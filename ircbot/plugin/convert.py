2920efe4a3c3da2a264c83893c6e8c78ce4b6a72
Andreas Lutro <anlutro@gmail.com>
2015-07-23 21:36:19 +0200

    add conversion plugin

---
+++ ircbot/plugin/convert.py
@@ -0,0 +1,35 @@
+import json
+import re
+import socket
+import urllib.error
+import urllib.request
+
+import ircbot.plugin
+
+
+def get_conversion_result(qs):
+	url = 'http://api.duckduckgo.com/?q='+qs+'&format=json'
+	try:
+		response = urllib.request.urlopen(url, timeout=2)
+		content = response.read().decode()
+	except (urllib.error.URLError, socket.timeout):
+		return False
+	data = json.loads(content)
+	if data['AnswerType'] == 'conversions' and data['Answer']:
+		return data['Answer']
+
+
+class ConversionPlugin(ircbot.plugin.Plugin):
+	pattern = re.compile(r'([\d.,]+) ([a-zA-Z]+) (into|in|to) ([a-zA-Z]+)')
+
+	@ircbot.plugin.reply()
+	def convert(self, msg):
+		match = self.pattern.search(msg.message)
+		if not match:
+			print(msg.message)
+			return
+
+		qs = '+'.join(match.groups())
+		result = get_conversion_result(qs)
+		if result:
+			return result
