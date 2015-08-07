2e19005929618542737fdaf8ebc487ccea7415be
Andreas Lutro <anlutro@gmail.com>
2015-08-07 20:41:30 +0200

    simplify run script

--- run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "-v" ] || [ "$1" = "--venv" ]; then
-	dir=$(dirname $(readlink -f $0))
-	source $dir/.virtualenv/bin/activate
-fi
-
-python -m unittest discover -s tests/ -p '*_test.py'
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "-v" ] || [ "$1" = "--venv" ]; then
-	dir=$(dirname $(readlink -f $0))
-	source $dir/.virtualenv/bin/activate
-fi
-
-python -m ircbot
