5ae57251d7cc5ec6387ee45adaab382c8298a31e
Andreas Lutro <anlutro@gmail.com>
2015-08-12 20:35:04 +0200

    use actual private functions

--- ircbot/__init__.py
+++ ircbot/__init__.py
@@ -13,6 +13,41 @@ import ircbot.plugin
 cfg = {}
 
 def run_bot(storage_dir, yml_config):
+	def configure_logging(log_level, log_path=None):
+		# set the level
+		root = logging.getLogger()
+		root.setLevel(log_level)
+
+		if log_path is None:
+			ch = logging.StreamHandler(sys.stdout)
+		else:
+			ch = logging.handlers.RotatingFileHandler(log_path, maxBytes=(1048576*5), backupCount=7)
+		ch.setLevel(log_level)
+
+		# define the logging format
+		formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(name)s - %(message)s')
+		ch.setFormatter(formatter)
+
+		# add the logging handler for all loggers
+		root.addHandler(ch)
+
+
+	def configure_plugins(bot, plugins_dict):
+		for name, plugin_class in plugins_dict.items():
+			# convenience compatibility layer for when plugins module was moved
+			plugin_class = plugin_class.replace('ircbot.plugin.', 'plugins.')
+
+			# dynamically import the plugin module and pass the class
+			parts = plugin_class.split('.')
+			module = importlib.import_module('.'.join(parts[:-1]))
+			plugin_class = getattr(module, parts[-1])
+			bot.register_plugin(name, plugin_class)
+
+
+	def configure_channels(bot, channel_dict):
+		for name, channel in channel_dict.items():
+			bot.add_channel(name, channel.get('plugins', []), channel.get('admins', []))
+
 	global cfg
 
 	with open(yml_config, 'r') as f:
@@ -22,7 +57,7 @@ def run_bot(storage_dir, yml_config):
 	log_level = logging.INFO
 	if 'log_level' in cfg:
 		log_level = getattr(logging, cfg.get('log_level').upper())
-	_configure_logging(log_level, cfg.get('log_path'))
+	configure_logging(log_level, cfg.get('log_path'))
 
 	# initialize the bot object
 	bot = ircbot.bot.Bot(
@@ -31,44 +66,8 @@ def run_bot(storage_dir, yml_config):
 		**cfg.get('bot', {})
 	)
 
-	_configure_plugins(bot, cfg.get('plugins', {}))
-	_configure_channels(bot, cfg.get('channels', {}))
+	configure_plugins(bot, cfg.get('plugins', {}))
+	configure_channels(bot, cfg.get('channels', {}))
 
 	# infinite loop go!
 	bot.run_forever()
-
-
-def _configure_logging(log_level, log_path=None):
-	# set the level
-	root = logging.getLogger()
-	root.setLevel(log_level)
-
-	if log_path is None:
-		ch = logging.StreamHandler(sys.stdout)
-	else:
-		ch = logging.handlers.RotatingFileHandler(log_path, maxBytes=(1048576*5), backupCount=7)
-	ch.setLevel(log_level)
-
-	# define the logging format
-	formatter = logging.Formatter('[%(asctime)s] %(levelname)s - %(name)s - %(message)s')
-	ch.setFormatter(formatter)
-
-	# add the logging handler for all loggers
-	root.addHandler(ch)
-
-
-def _configure_plugins(bot, plugins_dict):
-	for name, plugin_class in plugins_dict.items():
-		# convenience compatibility layer for when plugins module was moved
-		plugin_class = plugin_class.replace('ircbot.plugin.', 'plugins.')
-
-		# dynamically import the plugin module and pass the class
-		parts = plugin_class.split('.')
-		module = importlib.import_module('.'.join(parts[:-1]))
-		plugin_class = getattr(module, parts[-1])
-		bot.register_plugin(name, plugin_class)
-
-
-def _configure_channels(bot, channel_dict):
-	for channel, cfg in channel_dict.items():
-		bot.add_channel(channel, cfg.get('plugins', []), cfg.get('admins', []))
