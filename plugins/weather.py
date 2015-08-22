13cb520d8b5a41ad93245e0154f74a72342cb260
Andreas Lutro <anlutro@gmail.com>
2015-08-22 16:09:17 +0200

    switch !weather to use openweathermap

--- plugins/weather.py
+++ plugins/weather.py
@@ -7,39 +7,30 @@ import ircbot.plugin
 import socket
 
 
-def get_wunderground_data(url):
+def make_http_request(url):
 	return urllib.request.urlopen(url, timeout=2).read().decode('utf-8')
 
 
 class WeatherPlugin(ircbot.plugin.Plugin):
 	@ircbot.plugin.command('weather')
 	def weather(self, cmd):
-		api_key = self.bot.config.get('wunderground_api_key')
-		if not api_key:
-			log.warning('Wunderground API key not set on config.yml!')
-			return
+		if len(cmd.args) < 1:
+			return 'Usage: !weather city'
 
-		if len(cmd.args) < 2:
-			return 'Usage: !weather city state/country'
-
-		city = '_'.join(cmd.args[:-1])
-		state_or_country = cmd.args[-1]
-		url = 'http://api.wunderground.com/api/{}/geolookup/conditions/q/{}/{}.json'.format(
-			api_key, city, state_or_country)
+		city = '-'.join(cmd.args)
+		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&unit=metric'.format(
+			city)
 
 		try:
-			response = get_wunderground_data(url)
+			response = make_http_request(url)
 		except (urllib.error.URLError, socket.timeout):
 			return 'An HTTP error occured, try again later!'
 
 		data = json.loads(response)
 
-		if 'error' in data['response']:
-			return 'Error: {}'.format(data['response']['error']['description'])
-
-		location = data['location']['city']
-		weather = data['current_observation']['weather'].lower()
-		temperature = data['current_observation']['temp_c']
+		location = '{}, {}'.format(data['name'], data['sys']['country'])
+		weather = data['weather'][0]['main']
+		temperature = data['main']['temp']
 
 		return 'Weather in {}: {} - the temperature is {}°C'.format(
 			location, weather, temperature)
--- tests/plugin/weather_test.py
+++ tests/plugin/weather_test.py
@@ -5,13 +5,13 @@ from tests.plugin import PluginTestCase
 import plugins.weather
 
 
-f = 'plugins.weather.get_wunderground_data'
+f = 'plugins.weather.make_http_request'
 
 
 def get_json(state):
 	path = os.path.join(
 		os.path.dirname(os.path.dirname(__file__)),
-		'files', 'wunderground_'+state+'.json'
+		'files', 'openweathermap_'+state+'.json'
 	)
 	with open(path, 'r') as f:
 		return f.read()
@@ -19,20 +19,14 @@ def get_json(state):
 
 class WeatherPluginTest(PluginTestCase):
 	def create_plugin(self):
-		self.bot.config.update({'wunderground_api_key': 'API_KEY'})
 		return plugins.weather.WeatherPlugin(self.bot, self.channel)
 
 	@mock.patch(f, return_value=get_json('edinburgh'))
-	def test_cmd_with_valid_city(self, mock):
+	def test_cmd_simple(self, mock):
 		ret = self.cmd('weather edinburgh uk')
-		self.assertEqual('Weather in Edinburgh: scattered clouds - the temperature is 17.2°C', ret)
+		self.assertEqual('Weather in Edinburgh, GB: Rain - the temperature is 17.64°C', ret)
 
 	@mock.patch(f, return_value=get_json('tel_aviv'))
 	def test_cmd_multiword_city(self, mock):
 		ret = self.cmd('weather tel aviv israel')
-		self.assertEqual('Weather in Tel Aviv: clear - the temperature is 33.4°C', ret)
-
-	@mock.patch(f, return_value=get_json('invalid'))
-	def test_cmd_with_invalid_city(self, mock):
-		ret = self.cmd('weather asdaf asdasd')
-		self.assertEqual('Error: No cities match your search query', ret)
+		self.assertEqual('Weather in Tel Aviv District, IL: Clouds - the temperature is 33.11°C', ret)
