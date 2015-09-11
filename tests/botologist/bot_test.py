1072097026ea643ead3d02f0a4f872dea8992800
Andreas Lutro <anlutro@gmail.com>
2015-09-11 13:06:40 +0200

    add tests for command matching

--- tests/botologist/bot_test.py
+++ tests/botologist/bot_test.py
@@ -1,7 +1,8 @@
 import unittest
+from unittest import mock
 
-import botologist.irc as irc
-import botologist.bot as bot
+from botologist import irc
+from botologist import bot
 
 
 class CommandMessageTest(unittest.TestCase):
@@ -10,3 +11,26 @@ class CommandMessageTest(unittest.TestCase):
 		cmd = bot.CommandMessage(msg)
 		self.assertEqual('!foo', cmd.command)
 		self.assertEqual(['bar', 'baz'], cmd.args)
+
+class BotTest(unittest.TestCase):
+	def test_matches_command_shorthand(self):
+		channel = bot.Channel('#chan')
+		def dummy_command_func(command):
+			return 'test: '+command.command
+		dummy_command_func._is_threaded = False
+		channel.commands['asdf'] = dummy_command_func
+		b = bot.Bot({'server': 'localhost:6667', 'storage_dir': '/tmp/botologist'})
+		b._send_msg = mock.MagicMock()
+
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!b'), channel)
+		b._send_msg.assert_not_called()
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!a'), channel)
+		b._send_msg.assert_called_with('test: !a', '#chan')
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!as'), channel)
+		b._send_msg.assert_called_with('test: !as', '#chan')
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!asd'), channel)
+		b._send_msg.assert_called_with('test: !asd', '#chan')
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!asdf'), channel)
+		b._send_msg.assert_called_with('test: !asdf', '#chan')
+		b._handle_command(irc.Message('foo!bar@baz', '#chan', '!asdfg'), channel)
+		b._send_msg.assert_not_called()
