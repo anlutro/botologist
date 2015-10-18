3dfd064e569fc58682729e90435d068b85fcd3c6
Andreas Lutro <anlutro@gmail.com>
2015-10-18 18:22:38 +0200

    make it easier to write custom local clients

--- botologist/protocol/local.py
+++ botologist/protocol/local.py
@@ -15,24 +15,25 @@ class Client(protocol.Client):
 		for callback in self.on_connect:
 			callback()
 
-		# the messages are always from the same user
-		user = User()
-		channel = next(iter(self.channels.values()))
+		self.user = User('user', 'user@localhost')
+		self.channel = next(iter(self.channels.values()))
+		self.input_loop()
 
+		log.info('Quitting local server')
+		for callback in self.on_disconnect:
+			callback()
+
+	def input_loop(self):
 		print('Running locally. Hit ^C, ^D or type "/exit" or "/quit" to quit.')
 		try:
 			in_str = input('>> ')
 			while in_str != '/exit' and in_str != '/quit':
-				message = Message(in_str, user, channel.name)
+				message = Message(in_str, self.user, self.channel.name)
 				for callback in self.on_privmsg:
 					callback(message)
 				in_str = input('>> ')
 		except (KeyboardInterrupt, EOFError):
 			print()
-
-		log.info('Quitting local server')
-		for callback in self.on_disconnect:
-			callback()
 
 	def send_msg(self, target, msg):
 		print('<< {}'.format(msg))
@@ -43,8 +44,7 @@ class Channel(protocol.Channel):
 
 
 class User(protocol.User):
-	def __init__(self):
-		super().__init__('user', 'user@localhost')
+	pass
 
 
 class Message(protocol.Message):
