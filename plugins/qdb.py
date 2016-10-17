9d4ede37339e4d21f8936f0deada90c1d66c8f72
Andreas Lutro <anlutro@gmail.com>
2016-10-17 13:02:46 +0200

    herp derp

--- plugins/qdb.py
+++ plugins/qdb.py
@@ -14,9 +14,9 @@ def _get_quote_url(quote):
 
 
 def _get_qdb_data(url, query_params):
-	response = requests.get(url, query_params, headers={'accept': 'application/json'}).json()
+	response = requests.get(url, query_params, headers={'accept': 'application/json'})
 	response.raise_for_status()
-	return response
+	return response.json()
 
 
 def _search_for_quote(quote):
