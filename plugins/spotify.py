e87d0d514cbcdc1da5a20859f6dcaf3961a7c142
Andreas Lutro <anlutro@gmail.com>
2017-06-29 21:39:08 +0200

    simplify

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -18,27 +18,22 @@ class Spotify:
 	def __init__(self, spotify=None):
 		self.spotipy = spotify or spotipy.Spotify()
 
-	def get_artist(self, artist_id):
-		data = self.spotipy.artist(artist_id)
-		return data['name'], ', '.join(data['genres'])
-
-	def get_album(self, album_id):
-		data = self.spotipy.album(album_id)
-		return _get_artist_str(data['artists']), data['name']
-
-	def get_track(self, track_id):
-		data = self.spotipy.track(track_id)
-		return _get_artist_str(data['artists']), data['album']['name'], data['name']
-
 	def get_info_str(self, item_type, item_id):
 		log.info('looking up spotify:%s:%s', item_type, item_id)
 		try:
 			if item_type == 'artist':
-				return '%s (%s)' % self.get_artist(item_id)
+				data = self.spotipy.artist(artist_id)
+				return '%s (%s)' % (data['name'], ', '.join(data['genres']))
 			elif item_type == 'album':
-				return '%s - %s' % self.get_album(item_id)
+				data = self.spotipy.album(album_id)
+				return '%s - %s' % (_get_artist_str(data['artists']), data['name'])
 			elif item_type == 'track':
-				return '%s - %s - %s' % self.get_track(item_id)
+				data = self.spotipy.track(track_id)
+				return '%s - %s - %s' % (
+					_get_artist_str(data['artists']),
+					data['album']['name'],
+					data['name'],
+				)
 			else:
 				raise ValueError('unknown item type: %r' % item_type)
 		except spotipy.client.SpotifyException:
