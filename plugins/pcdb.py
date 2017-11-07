41ba9d1339aa044674754cc900193ea3a2c4f5bf
Anton <moopie@users.noreply.github.com>
2017-11-07 15:23:20 +0200

    shitty python globals

--- plugins/pcdb.py
+++ plugins/pcdb.py
@@ -30,6 +30,7 @@ class PcdbPlugin(botologist.plugin.Plugin):
 
 	@botologist.plugin.command('pcdb', alias=['random', 'r'])
 	def get_pcdb_random(self, cmd):
+		global redis_storage_item
 		include_url = False
 		if cmd.args and cmd.args[-1] in ('+url', '--url', '-u'):
 			include_url = True
