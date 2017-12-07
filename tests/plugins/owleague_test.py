c8b0f08df0fc1082fd20107d80b2c4690f76d12d
Andreas Lutro <anlutro@gmail.com>
2017-12-07 10:55:06 +0100

    more tests

--- tests/plugins/owleague_test.py
+++ tests/plugins/owleague_test.py
@@ -23,15 +23,15 @@ class OwleaguePluginTest(PluginTestCase):
 					'liveStatus': 'UPCOMING',
 					'startDate': '2017-12-10T15:00:00Z+00:00',
 					'competitors': [
-						{'name': 'team1'},
-						{'name': 'team2'},
+						{'name': 'team3'},
+						{'name': 'team4'},
 					],
 				}
 			}
 		}
 		with mock.patch(f, return_value=data):
 			ret = self.cmd('owl')
-		self.assertEqual(ret, 'Live now: team1 vs team2 -- Next match: team1 vs team2 at 2017-12-10 15:00 +0000 -- https://overwatchleague.com')
+		self.assertEqual(ret, 'Live now: team1 vs team2 -- Next match: team3 vs team4 at 2017-12-10 15:00 +0000 -- https://overwatchleague.com')
 
 	def test_returns_next_match(self):
 		data = {
@@ -50,3 +50,97 @@ class OwleaguePluginTest(PluginTestCase):
 		with mock.patch(f, return_value=data):
 			ret = self.cmd('owl')
 		self.assertEqual(ret, 'Next match: team1 vs team2 at 2017-12-10 15:00 +0000 -- https://overwatchleague.com')
+
+	def test_ticker_does_not_repeat_itself(self):
+		data = {
+			'data': {
+				'liveMatch': {
+					'liveStatus': 'LIVE',
+					'competitors': [
+						{'name': 'team1'},
+						{'name': 'team2'},
+					],
+				},
+				'nextMatch': {
+					'liveStatus': 'UPCOMING',
+					'startDate': '2017-12-10T15:00:00Z+00:00',
+					'competitors': [
+						{'name': 'team3'},
+						{'name': 'team4'},
+					],
+				}
+			}
+		}
+		with mock.patch(f, return_value=data):
+			ret1 = self.plugin.ticker()
+			ret2 = self.plugin.ticker()
+		self.assertTrue(ret1)
+		self.assertFalse(ret2)
+
+	def test_ticker_does_not_trigger_on_next_match_time_change(self):
+		data = {
+			'data': {
+				'liveMatch': {
+					'liveStatus': 'LIVE',
+					'competitors': [
+						{'name': 'team1'},
+						{'name': 'team2'},
+					],
+				},
+				'nextMatch': {
+					'liveStatus': 'UPCOMING',
+					'startDate': '2017-12-10T15:00:00Z+00:00',
+					'competitors': [
+						{'name': 'team3'},
+						{'name': 'team4'},
+					],
+				}
+			}
+		}
+
+		with mock.patch(f, return_value=data):
+			ret1 = self.plugin.ticker()
+
+		data['data']['nextMatch']['startDate'] = '2017-12-10T16:00:00Z+00:00'
+
+		with mock.patch(f, return_value=data):
+			ret2 = self.plugin.ticker()
+
+		self.assertTrue(ret1)
+		self.assertFalse(ret2)
+
+	def test_ticker_updates_when_match_changes(self):
+		data = {
+			'data': {
+				'liveMatch': {
+					'liveStatus': 'LIVE',
+					'competitors': [
+						{'name': 'team1'},
+						{'name': 'team2'},
+					],
+				},
+				'nextMatch': {
+					'liveStatus': 'UPCOMING',
+					'startDate': '2017-12-10T15:00:00Z+00:00',
+					'competitors': [
+						{'name': 'team3'},
+						{'name': 'team4'},
+					],
+				}
+			}
+		}
+
+		with mock.patch(f, return_value=data):
+			ret1 = self.plugin.ticker()
+
+		data['data']['liveMatch']['competitors'][0]['name'] = 'team3'
+		data['data']['liveMatch']['competitors'][1]['name'] = 'team4'
+		data['data']['nextMatch']['competitors'][0]['name'] = 'team5'
+		data['data']['nextMatch']['competitors'][1]['name'] = 'team6'
+
+		with mock.patch(f, return_value=data):
+			ret2 = self.plugin.ticker()
+
+		self.assertTrue(ret1)
+		self.assertTrue(ret2)
+		self.assertNotEqual(ret1, ret2)
